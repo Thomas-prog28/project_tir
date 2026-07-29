ce43ef95244f528bf0b3dd3ae19d7476dcd6e320
ThomasGirard <thomasg.pro@live.fr>
2026-07-29 17:01:07 +0200

    travail des media query sur le menu

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@ import './js/header.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/layout/_header.scss';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
