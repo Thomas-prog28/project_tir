2e20fd505ab17fbebb259d008bc21d5e6f9b67ad
ThomasGirard <thomasg.pro@live.fr>
2026-07-30 12:38:09 +0200

    fin travail sur le header en toute taille, début page d'accueil

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,8 @@
+//AFFICHAGE DU MENU CACHE (<1024px) EN CLIQUANT SUR LE BOUTON BURGER
 const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__menu');
+const nav = document.querySelector('.header__nav');
 
 burger.addEventListener('click', () => {
-    const isOpen = menu.classList.toggle('header__menu--open');
+    const isOpen = nav.classList.toggle('header__nav--open');
     burger.setAttribute('aria-expanded', isOpen);
 })
